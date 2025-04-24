03e025d2a0eef0e08cb1effc037c3d5a9d43c6ae
mohan <nandivada.mohankrishna@gmail.com>
2025-04-24 04:59:07 +0000

    second push in GCP

--- devops.sh
+++ devops.sh
@@ -1,2 +1,3 @@
 practising cloning in git using ssh remote origin
 pushed the code from cloudshell to github now doing changes in github and then will do pull activity in cloudshell
+completed pull in cloud shell now again doing changes in cloudshell and will push to github
