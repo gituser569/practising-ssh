de6ffe2c78ae4eaa1b0de9bfb39e0513cb2c3299
mohan <nandivada.mohankrishna@gmail.com>
2025-04-24 10:42:13 +0530

    final

--- devops.sh
+++ devops.sh
@@ -4,3 +4,4 @@ completed pull in cloud shell now again doing changes in cloudshell and will pus
 completed second push and now doing changes in github and try to clone in gitbash
 completed git clone using ssh key in git bash also now going to push grom gitbash to github
 completed push from git bash to git hub now doing changes in github and will do pull in gitbash
+done 
