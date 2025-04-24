3adee8a9e8393039ef57c38e2fb52f03998311db
gituser569 <nandivada.mohankrishna@gmail.com>
2025-04-24 10:30:59 +0530

    trying to clone in gitbash devops.sh

--- devops.sh
+++ devops.sh
@@ -1,3 +1,4 @@
 practising cloning in git using ssh remote origin
 pushed the code from cloudshell to github now doing changes in github and then will do pull activity in cloudshell
 completed pull in cloud shell now again doing changes in cloudshell and will push to github
+completed second push and now doing changes in github and try to clone in gitbash
