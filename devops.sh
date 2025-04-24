fc9fd042c95e3f7d303edd603eff1c59ec5db5a8
gituser569 <nandivada.mohankrishna@gmail.com>
2025-04-24 10:40:27 +0530

    trying to do pull in gitbash

--- devops.sh
+++ devops.sh
@@ -3,3 +3,4 @@ pushed the code from cloudshell to github now doing changes in github and then w
 completed pull in cloud shell now again doing changes in cloudshell and will push to github
 completed second push and now doing changes in github and try to clone in gitbash
 completed git clone using ssh key in git bash also now going to push grom gitbash to github
+completed push from git bash to git hub now doing changes in github and will do pull in gitbash
